4ab2f20edfb91eeb111182d7fca66746937c4167
cich-adrian <156018491+cich-adrian@users.noreply.github.com>
2024-01-13 19:41:24 +0100

    Add files via upload

---
+++ rownanie_kwadratowe.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+	float a, b, c, d, x, x1, x2;
+
+	cout << "\n Podaj współczynnik a" << endl;
+	cin >> a;
+
+	cout << "Podaj współczynnik b" << endl;
+	cin >> b;
+
+	cout << "Podaj współczynnik c" << endl;
+	cin >> c;
+
+	d = b * b - 4 * a * c;
+
+	if (d == 0)
+	{
+		x1 = (-b) / 2 * a;
+		x2 = 0;
+		cout <<"pierwiastek to:" <<	 x1 << endl;
+	}
+	else if (d > 0)
+	{
+		x1 = (-b + sqrt(d)) / 2 * a;
+		x2 = (-b - sqrt(d)) / 2 * a;
+		cout << "pierwszy pierwiastek to:" <<	 x1 << endl << "drugi pierwiastek to:" <<	 x2;
+	}
+	else
+
+	{
+		cout << "Brak rozwiązań";
+	}
+
+}
